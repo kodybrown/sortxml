b70ff9665a6c7b9782141fd5766699db30c26fd1
Kody Brown <kody@bricksoft.com>
2014-02-12 15:48:49 -0700

    - added attribute sorting..   still needs a bit more work, to check children nodes/attributes when the current node's attributes all match. - added commandline parameter for specifying case-sensitivity:   /i --case-insensitive   !i --case-sensitive

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace sortxml
         static bool sort_node = true,
             sort_attr = true,
             pretty = true;
+        static StringComparison
+            sort_node_comp = StringComparison.CurrentCulture, // Default to case-sensitive sorting.
+            sort_attr_comp = StringComparison.CurrentCulture;
 
         static int Main(string[] arguments)
         {
@@ -31,7 +34,17 @@ namespace sortxml
                     }
                     string al = a.ToLower();
 
-                    if (al.Equals("sort") || al.StartsWith("sortall") || al.StartsWith("sort-all"))
+                    if (al.Equals("i") || al.StartsWith("casei") || al.StartsWith("case-i"))
+                    {
+                        sort_node_comp = StringComparison.CurrentCultureIgnoreCase;
+                        sort_attr_comp = StringComparison.CurrentCultureIgnoreCase;
+                    }
+                    else if (al.Equals("!i") || al.StartsWith("cases") || al.StartsWith("case-s"))
+                    {
+                        sort_node_comp = StringComparison.CurrentCulture;
+                        sort_attr_comp = StringComparison.CurrentCulture;
+                    }
+                    else if (al.Equals("sort") || al.StartsWith("sortall") || al.StartsWith("sort-all"))
                     {
                         sort_node = true;
                         sort_attr = true;
@@ -41,7 +54,6 @@ namespace sortxml
                         sort_node = false;
                         sort_attr = false;
                     }
-
                     else if (al.StartsWith("sortn") || al.StartsWith("sort-n"))
                     {
                         sort_node = true;
@@ -50,7 +62,6 @@ namespace sortxml
                     {
                         sort_node = false;
                     }
-
                     else if (al.StartsWith("sorta") || al.StartsWith("sort-a"))
                     {
                         sort_attr = true;
@@ -59,7 +70,6 @@ namespace sortxml
                     {
                         sort_attr = false;
                     }
-
                     else if (al.StartsWith("pretty"))
                     {
                         pretty = true;
@@ -104,9 +114,13 @@ namespace sortxml
                 return 100;
             }
 
-            if (sort_node || sort_attr)
+            if (sort_attr)
             {
-                sort(doc.DocumentElement);
+                SortNodeAttrs(doc.DocumentElement);
+            }
+            if (sort_node)
+            {
+                SortNodes(doc.DocumentElement);
             }
 
             if (outf.Length > 0)
@@ -130,18 +144,20 @@ namespace sortxml
             return 0;
         }
 
-        static void sort(XmlNode node)
+        static void SortNodes(XmlNode node)
         {
-            if (sort_attr)
+            // Go down to the furthest child and start there..
+            // That is so I can include child nodes in the current node's sort,
+            // if all of it's attributes match..
+            for (int i = 0, len = node.ChildNodes.Count; i < len; i++)
             {
-                sortattr(node);
+                SortNodes(node.ChildNodes[i]);
             }
 
             // Remove, sort, then re-add the node's children.
             if (sort_node && node.ChildNodes != null && node.ChildNodes.Count > 0)
             {
                 List<XmlNode> nodes = new List<XmlNode>(node.ChildNodes.Count);
-                int result;
 
                 for (int i = node.ChildNodes.Count - 1; i >= 0; i--)
                 {
@@ -149,53 +165,118 @@ namespace sortxml
                     node.RemoveChild(node.ChildNodes[i]);
                 }
 
-                nodes.Sort(delegate(XmlNode a, XmlNode b)
-                {
-                    result = string.Compare(a.Name, b.Name, StringComparison.CurrentCultureIgnoreCase);
-                    if (result == 0)
-                    {
-                        // todo: must include the attributes in the sorting..
-                    }
-                    return result;
-                });
+                nodes.Sort(SortDelegate);
 
                 for (int i = 0; i < nodes.Count; i++)
                 {
                     node.AppendChild(nodes[i]);
                 }
             }
-
-            for (int i = 0, len = node.ChildNodes.Count; i < len; i++)
-            {
-                sort(node.ChildNodes[i]);
-            }
         }
 
-        static void sortattr(XmlNode node)
+        static int SortDelegate(XmlNode a, XmlNode b)
+        {
+            XmlAttribute aa, bb;
+            XmlAttributeCollection col1, col2;
+            int result;
+
+            result = string.Compare(a.Name, b.Name, sort_node_comp);
+            // NOTE: Always sort the _nodes_ based on its attributes (when the 
+            //       name matches), but don't actually sort the node's attributes.
+            //       (Sorting attributes is done before node sorting happens,
+            //       if specified).
+            if (result == 0)
+            {
+                col1 = (a.Attributes.Count >= b.Attributes.Count) ? a.Attributes : b.Attributes;
+                col2 = (a.Attributes.Count >= b.Attributes.Count) ? b.Attributes : a.Attributes;
+
+                for (int i = 0; i < col1.Count; i++)
+                {
+                    if (i < col2.Count)
+                    {
+                        aa = col1[i];
+                        bb = col2[i];
+                        result = string.Compare(aa.Name, bb.Name, sort_attr_comp);
+                        if (result == 0)
+                        {
+                            result = string.Compare(aa.Value, bb.Value, sort_attr_comp);
+                            if (result != 0)
+                            {
+                                return result;
+                            }
+                            // Attribute name and value match.. continue loop.
+                        }
+                        else
+                        {
+                            return result;
+                        }
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+
+                // If we get here, that means that the node's attributes (and values) all match..
+                // TODO: Should we go down into the child node collections for sorting?
+                //       See example `c.xml`..
+                Console.WriteLine(a.Name + "==" + b.Name + " all attributes matched");
+            }
+
+            return result;
+        }
+
+        static void SortNodeAttrs(XmlNode node)
         {
             // Remove, sort, then re-add the node's attributes.
             if (sort_attr && node.Attributes != null && node.Attributes.Count > 0)
             {
-                List<XmlAttribute> attrs = new List<XmlAttribute>(node.Attributes.Count);
+                SortXmlAttributeCollection(node.Attributes);
+            }
 
-                for (int i = node.Attributes.Count - 1; i >= 0; i--)
+            // Sort the children node's attributes also.
+            for (int i = 0, len = node.ChildNodes.Count; i < len; i++)
+            {
+                SortNodeAttrs(node.ChildNodes[i]);
+            }
+        }
+
+        static void SortXmlAttributeCollection(XmlAttributeCollection col)
+        {
+            // Remove, sort, then re-add the attributes to the collection.
+            if (sort_attr && col != null && col.Count > 0)
+            {
+                List<XmlAttribute> attrs = new List<XmlAttribute>(col.Count);
+
+                for (int i = col.Count - 1; i >= 0; i--)
                 {
-                    attrs.Add(node.Attributes[i]);
-                    node.Attributes.RemoveAt(i);
+                    attrs.Add(col[i]);
+                    col.RemoveAt(i);
                 }
 
-                attrs.Sort(delegate(XmlAttribute a, XmlAttribute b)
-                {
-                    return string.Compare(a.Name, b.Name, StringComparison.CurrentCultureIgnoreCase);
-                });
+                SortAttributeList(attrs);
 
                 for (int i = 0; i < attrs.Count; i++)
                 {
-                    node.Attributes.Append(attrs[i]);
+                    col.Append(attrs[i]);
                 }
             }
         }
 
+        static void SortAttributeList(List<XmlAttribute> attrs)
+        {
+            int result;
+
+            attrs.Sort(delegate(XmlAttribute a, XmlAttribute b)
+            {
+                result = string.Compare(a.Name, b.Name, sort_attr_comp);
+                if (result == 0)
+                {
+                    return string.Compare(a.Value, b.Value, sort_attr_comp);
+                }
+                return result;
+            });
+        }
 
         static void usage()
         {
